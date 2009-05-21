7fd88261188cd20577ca7116e4abfa6014450814
Nat Welch <nat@natwelch.com>
2009-05-21 02:01:44 -0700

    Useless work. Doesn't even "compile"

--- rsync.py
+++ rsync.py
@@ -5,19 +5,26 @@ from __future__ import with_statement
 from errno import EACCES
 from os.path import realpath
 from sys import argv, exit
+from xml.dom.minidom import parse
+from xml.dom.minidom import Document
 
 import os
 
 from fuse import FUSE, Operations, LoggingMixIn
 from metadata import Metadata
 
-class Git(LoggingMixIn, Operations):    
-		
-	def __init__(remote,remotedir):
+class Rsync():    
+	def __init__(self, remotehost):	
+		self.__init__(self, remotehost,"/fuse")
+
+	def __init__(self, remote, remotedir):
 		self.remotehost = remote
 		self.remotedir = remotedir
 
-	def push(filename)
-		command = "rsync -azc " + filename + " " + self.remotehost + ":" + self.remotedir + filename
+	def push(self, filename):
+		# a : Archival
+		# z : compression
+		# --safe-links : don't get symlinks
+		command = "rsync -az --safe-links " + filename + " " + self.remotehost + ":" + self.remotedir + filename
 		os.system(command)
 	
